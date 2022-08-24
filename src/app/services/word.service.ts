b6ea8bf7c373097948764e05fb485609f4917209
Igor <igor.borowski@capgemini.com>
2022-08-24 16:11:09 +0200

    create putword & small changes

--- src/app/services/word.service.ts
+++ src/app/services/word.service.ts
@@ -8,17 +8,17 @@ import { Word } from '../interfaces/word';
 export class WordService {
 
   server: string = 'http://localhost:3000/vocabulary'
+  header: Object = { 'content-type': 'application/json'}
 
   constructor(private http: HttpClient) { }
 
   getWords() {
     return this.http.get(this.server)
   }
-  postWord(body: Word | object) {
-    const header: Object = { 'content-type': 'application/json'} 
-    return this.http.post(this.server, body, header)
+  postWord(body: Word | object) { 
+    return this.http.post(this.server, body, this.header)
   }
-  putWord(body: Word | object) {
-    
+  putWord(body: Word | Object) {
+    return this.http.put(this.server, body, this.header)
   }
 }
